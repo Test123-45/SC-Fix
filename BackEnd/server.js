6d51a7feff59f2fb0a3978b8afa9bec56df5b3ca
Stanly <86126384+StepSisStuck@users.noreply.github.com>
2025-02-02 09:11:42 +0000

    Fix .appref-ms file in public folder
    Remove the `.appref-ms` file from the `BackEnd/public` directory and restrict access to such files in `server.js`.
    
    * **Remove File**
      - Delete `BackEnd/public/GitHub-1609999590009.appref-ms`.
    
    * **Restrict Access**
      - Add middleware in `BackEnd/server.js` to block access to `.appref-ms` files by returning a `403 Forbidden` response.
      - Place the middleware at the top of the file before other middleware.

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,15 @@
-
-
 var express = require('express');
 var serveStatic = require('serve-static');
 var app = require('./controller/app.js');
 
 var port = 8081;
+
+app.use((req, res, next) => {
+    if (req.url.endsWith('.appref-ms')) {
+        return res.status(403).send('Access Forbidden');
+    }
+    next();
+});
 
 app.use(serveStatic(__dirname + '/public')); 
 
@@ -13,4 +18,3 @@ var server = app.listen(port, function(){
 });
 
 var app = require('./controller/app.js');
-
